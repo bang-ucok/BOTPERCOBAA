277945aa88e696d5d2f646728e32c87d57262915
Kenichi-BOTZ <106317066+Kenichi-BOTZ@users.noreply.github.com>
2022-06-22 23:55:26 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,32 +1,32 @@
 //=============『 Utama 』================== //
-global.owner = ['6287892711054'] 
+global.owner = ['62895388375000'] 
 global.mods = [] 
 global.prems = [] 
 
 //=============『 Info Owner 』============== //
-global.nameowner = 'Hyzer Official'
+global.nameowner = 'Kenichi'
 global.numberowner = '6287892711054'
-global.instagram = 'https://instagram.com/hyzer_stfu'
-global.github = 'https://github.com/Hyzerr'
-global.dana = '087892711054'
-global.pulsa = '085212023766'
-global.gopay = '087892711054'
+global.instagram = 'https://instagram.com/kenibotz_kenichi'
+global.github = 'https://github.com/Kenichi-BOTZ'
+global.dana = '085161533232'
+global.pulsa = '085161533232'
+global.gopay = '085161533232'
 
 //=============『 Info Bot 』=================//
-global.namebot = 'ShiroBot-Md'
-global.gc = 'https://chat.whatsapp.com/Jzd9DEVB5nODtNBk1VCNrV'
-global.web = 'https://hyzerr-apii.herokuapp.com' //ubah jadi website lu, bisa link ig, link github, link yt, klo link gc ntr beda tampilan lagi. 
+global.namebot = 'KeniBotz-Md'
+global.gc = 'https://chat.whatsapp.com/LBHHmgLnr9Y5q7ZkhGikkI'
+global.web = 'https://github.com/Kenichi-BOTZ' //ubah jadi website lu, bisa link ig, link github, link yt, klo link gc ntr beda tampilan lagi. 
 global.price1 = '1 bulan = 10.000\n2 Minggu 7.000\n1 Minggu 5.000'
 
 //=======『 Tampilan Dan Lainnya 』============//
-global.fotonya1 = 'https://telegra.ph/file/e4a2f4339da8a32ad20a1.jpg' //ganti jadi foto bot mu
-global.fotonya2 = 'https://telegra.ph/file/8c471d45b6cedad43cf84.jpg' //ini juga ganti 
+global.fotonya1 = 'https://telegra.ph/KeniBotz-Logo-06-21' //ganti jadi foto bot mu
+global.fotonya2 = 'https://telegra.ph/KeniBotz-Logo-06-21' //ini juga ganti 
 global.lolkey = 'Papah-Chan' //biar mudah ngegantinya semisal apikeynya expired:v
 global.zenzkey = 'BagasPrdn' //ganti jadi apikey lu kalau expired
-global.wm = '𝚂𝙷𝙸𝚁𝙾-𝙼𝙳'
+global.wm = 'Kenichi-Md'
 global.watermark = wm
-global.wm2 = '                     「 𝚂𝙷𝙸𝚁𝙾-𝙼𝙳 あ⁩ 」'
-global.wm3 = '⫹⫺ 𝚂𝙷𝙸𝚁𝙾-𝙼𝙳'
+global.wm2 = '                     「 kenibotz あ⁩ 」'
+global.wm3 = '⫹⫺ kenibotz-md'
 global.htki = '––––––『' 
 global.htka = '』––––––'
 global.media = 'https://telegra.ph/file/72168c6f1aaa90315c369.jpg'
@@ -37,7 +37,7 @@ global.benar = 'Benar ✅\n'
 global.salah = 'Salah ❌\n'
 global.stiker_wait = 'Stiker sedang dibuat'
 global.packname = '2022'
-global.author = '© Hyzer Official'
+global.author = '© kenichi offc'
 
 //=============『 Apikey 』================== //
 global.APIs = { // API Prefix
